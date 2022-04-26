4d4ea96567a78f43b1bd4219ee99c478f727d312
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-26 15:47:26 +0530

    argument decorator

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
+// function log(target: any, key: any, descriptor: any) {
+//   console.log(`${key} was called`);
+// }
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+// class calculator {
+//   //using the decorator @log
+//   @log
+//   square(n: number) {
+//     return n * n;
+//   }
+// }
+//Arguments in method Decorators
 function log(target, key, descriptor) {
-    console.log("".concat(key, " was called"));
+    //   console.log("target", target);
+    var original = descriptor.value;
+    descriptor.value = function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        //call the original method
+        var result = original.apply(this, args);
+        //log the call, and the result
+        console.log("".concat(key, " with args ").concat(JSON.stringify(args), " returned ").concat(JSON.stringify(result)));
+        return result;
+    };
+    return descriptor;
 }
 var calculator = /** @class */ (function () {
     function calculator() {
     }
+    //using the decorator @log
     calculator.prototype.square = function (n) {
         return n * n;
     };
@@ -18,3 +43,6 @@ var calculator = /** @class */ (function () {
     ], calculator.prototype, "square", null);
     return calculator;
 }());
+var calculators = new calculator();
+calculators.square(2);
+calculators.square(4);
--- app.ts
+++ app.ts
@@ -1,5 +1,31 @@
+// function log(target: any, key: any, descriptor: any) {
+//   console.log(`${key} was called`);
+// }
+
+// class calculator {
+//   //using the decorator @log
+//   @log
+//   square(n: number) {
+//     return n * n;
+//   }
+// }
+
+//Arguments in method Decorators
 function log(target: any, key: any, descriptor: any) {
-  console.log(`${key} was called`);
+  //   console.log("target", target);
+  const original = descriptor.value;
+  descriptor.value = function (...args: any[]) {
+    //call the original method
+    const result = original.apply(this, args);
+    //log the call, and the result
+    console.log(
+      `${key} with args ${JSON.stringify(args)} returned ${JSON.stringify(
+        result
+      )}`
+    );
+    return result;
+  };
+  return descriptor;
 }
 
 class calculator {
@@ -9,3 +35,8 @@ class calculator {
     return n * n;
   }
 }
+
+const calculators = new calculator();
+calculators.square(2);
+
+calculators.square(4);
